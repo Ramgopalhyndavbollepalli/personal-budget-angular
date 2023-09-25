c574de714f635eef3ad30a2c4e43c77618e3b7e0
Ramgopalhyndav Bollepalli <ramgopalhyndavbollepalli@RamgopandavsMBP.lan>
2023-09-24 23:03:27 -0400

    added instructions

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { ContactComponent } from './contact/contact.component';
 
@@ -34,7 +35,7 @@ import { ContactComponent } from './contact/contact.component';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
